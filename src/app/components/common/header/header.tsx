6889b02817dfe4115fc71bbccae6d13474cc6be0
unknown <shahin.n14001995@gmail.com>
2025-09-29 13:26:33 +0330

    feat: header items set

--- src/app/components/common/header/header.tsx
+++ src/app/components/common/header/header.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import Image from "next/image";
 
-export const Header = () =>{
-    return <>header</>
-}
+import { Menu } from "@mui/icons-material";
+import AppBar from "@mui/material/AppBar";
+import Badge from "@mui/material/Badge";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+
+import { CartLogo } from "@/app/assets/images/cart";
+import Logo from "@/app/assets/images/header-logo.png";
+
+export const Header = () => {
+  return (
+    <AppBar position="fixed" elevation={0} sx={{ bgcolor: "white", borderBottom: "1px solid #eee" }}>
+      <Toolbar component={Container}>
+        <IconButton size="large" edge="start" aria-label="menu">
+          <Menu />
+        </IconButton>
+        <Box sx={{ flexGrow: 1, textAlign: "center" }}>
+          <Image src={Logo.src} width={Logo.width} height={Logo.height} alt="مردادینو" />
+        </Box>
+        <IconButton size="large" aria-label="cart">
+          <Badge
+            badgeContent={0}
+            color="primary"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <CartLogo sx={{ fontSize: 28 }} />
+          </Badge>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
