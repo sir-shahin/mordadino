02c0b33e509bb87493200c880c8f0d8550079bf2
unknown <shahin.n14001995@gmail.com>
2025-09-29 11:21:35 +0330

    feat: primary color added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "@/providers";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "MORDADINO | مردادینو",
-  description: "نقدانه: همه چیز زیر ذره‌بین شما",
+  description: "مردادینو: محصولتو از هر جای دنیا پیدا کن",
 };
 
 export default function RootLayout({
@@ -12,10 +13,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const primaryColor = process.env.NEXT_PUBLIC_PRIMARY_COLOR;
   return (
     <html lang="fa" suppressHydrationWarning>
       <body suppressHydrationWarning>
         <Providers>
+          <NextTopLoader color={primaryColor} showSpinner={false} />
+          {/* <Header /> */}
+
           {children}
         </Providers>
       </body>
